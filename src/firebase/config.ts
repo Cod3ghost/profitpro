7a7139f192aefaa41da4866157669f8009803e78
Desmond Amos Bature <desbature@gmail.com>
2025-10-14 12:53:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2854568318-40960",
+  "appId": "1:105506237421:web:63363f3f2ae41b349555e4",
+  "apiKey": "AIzaSyCYv9tsMZLVMYGB7Rb3NqLMXmRE9nsU6fY",
+  "authDomain": "studio-2854568318-40960.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "105506237421"
+};
